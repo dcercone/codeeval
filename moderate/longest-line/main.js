4623d20f4f130cd1285253aa269f7a2324b39ce3
DJ Cercone <dcercone@gmail.com>
2016-09-30 15:40:21 -0400

    Remove string length array

--- moderate/longest-line/main.js
+++ moderate/longest-line/main.js
@@ -1,21 +1,15 @@
-/*Sample code to read in test cases: */
-
 function swap(i,l,r){
 	var _t = i[l];
 	i[l] = i[r];
-	i[r] = _t;	
-
-	var temp = w[l];
-	w[l] = w[r];
-	w[r] = temp;
+	i[r] = _t;
 }
 function partition(i,l,r){
-	var p = i[Math.floor((l+r)/2)];
+	var p = i[Math.floor((l+r)/2)].length;
 	while(l <= r){
-		while(i[l] > p){
+		while(i[l].length > p){
 			l++;
 		}
-		while(i[r] < p){
+		while(i[r].length < p){
 			r--;
 		}
 		if(l <= r){
@@ -41,19 +35,18 @@ function quicksort(i,l,r){
 	return i;
 }
 
-var fs  = require("fs"), t = undefined, w = new Array(), n = new Array();
+var fs  = require("fs"), t = undefined, w = new Array();
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (l) {
     if (l != "") {
     	if (t === undefined) {
     		t = parseInt(l[0]);
     	} else{
     		w.push(l);
-    		n.push(l.length);
     	}
     }
 });
 
-quicksort(n,0,(n.length-1));
+quicksort(w,0,(w.length-1));
 
 for(var i = 0; i < t; i++){
 	console.log(w[i].toString());
